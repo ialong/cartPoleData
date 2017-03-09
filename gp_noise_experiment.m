5c7a09c028a4c3423289876a52815157c00bbfec
Alessandro Davide Ialongo <alex.ialongo@gmail.com>
2017-03-09 12:16:36 +0000

    different noise addition process - save noiseless data

--- gp_noise_experiment.m
+++ gp_noise_experiment.m
@@ -1,60 +1,78 @@
-addpath(genpath('/home/adi24/gpml-matlab-v4.0-2016-10-19'))
-
-meanfunc = {@meanSum, {@meanLinear, @meanConst}};          
-covfunc = @covSEard;             
-likfunc = @likGauss;             
+addpath(genpath('/home/adi24/gpml-matlab-v4.0-2016-10-19'))           
 
 series = 20;
 series_length = 100;
 discard_first_n_steps = 0;
 
+%% Generate (noiseless) data:
+
 rng(1)
 clear odestep
-[data_noiseless, ~, ~, plant] = return_cartpole_data(series, series_length, discard_first_n_steps, 0);
+[data, ~, ~, plant] = return_cartpole_data(series, series_length, discard_first_n_steps, 0);
+   
+% truncate series so cart lies in [-1, 1]meters:
+stacked_x = [];
+stacked_y = [];
+boundaries = zeros(series,1);
+for n = 1:series
+    data(n).y(:,1) = data(n).y(:,1) - data(n).y(1,1);
+    pos_less_than_one = abs(data(n).y(:,1)) > 1;
+    pos_less_than_one = find(pos_less_than_one, 1, 'first') - 1;
+    if isempty(pos_less_than_one); pos_less_than_one = series_length; end
+    boundaries(n) = pos_less_than_one;
+    data(n).y = data(n).y(1:pos_less_than_one,:);
+
+    data(n).u = [0; data(n).u];
+    data(n).u = data(n).u(1:pos_less_than_one,:);
+
+    % stack data all together:
+    x_and_u = [data(n).y(1:end-1,:)   data(n).u(1:end-1)   data(n).u(2:end)];
+    stacked_x = [stacked_x; x_and_u];
+    stacked_y = [stacked_y; data(n).y(2:end,:)];
+end
+boundaries = cumsum(boundaries);
+
+% split beteern train and test:
+train_test_cutoff = ceil(size(stacked_x,1)/2);
+
+train_x_noiseless = stacked_x(1:train_test_cutoff,:);
+train_y_noiseless = stacked_y(1:train_test_cutoff,:);
+test_x_noiseless = stacked_x(train_test_cutoff+1:end,:);
+test_y_noiseless = stacked_y(train_test_cutoff+1:end,:);
+
+
+
+%% Fit (noisy) data:
 
 % Noise s.d.'s: 1cm, 1 degree
 % Dimensions: x position, sin(\theta), cos(\theta), cart velocity, angular velocity
 obs_noise_stds = [0.01 pi/180 pi/180 0.01/plant.dt pi/180/plant.dt]; % add noise to sin & cos independently
 
-noise_std_multipliers = [0 0.1 1 2 3 5 10 20];
+noise_std_multipliers = [0 0.1 1 2 3 5 10 20 50];
 
 for noise_std_multiplier = noise_std_multipliers
     noise_std_multiplier
     
-    data = data_noiseless;
+    % add noise to data:
+    stacked_x_noisy = stacked_x;
+    stacked_y_noisy = stacked_y;
     
-    % truncate series so cart lies in [-1, 1]meters:
-    stacked_x = [];
-    stacked_y = [];
-    for n = 1:series
-        data(n).y(:,1) = data(n).y(:,1) - data(n).y(1,1);
-        pos_less_than_one = abs(data(n).y(:,1)) > 1;
-        pos_less_than_one = find(pos_less_than_one, 1, 'first') - 1;
-        if isempty(pos_less_than_one); pos_less_than_one = series_length; end
-        data(n).y = data(n).y(1:pos_less_than_one,:);
-        
-        data(n).u = [0; data(n).u];
-        data(n).u = data(n).u(1:pos_less_than_one,:);
-        
-        % add noise to data:
-        additive_noise = bsxfun(@times, randn(size(data(n).y)), noise_std_multiplier*obs_noise_stds);
-        data(n).y = data(n).y + additive_noise;
-        
-        % stack data all together:
-        x_and_u = [data(n).y(1:end-1,:)   data(n).u(1:end-1)   data(n).u(2:end)];
-        stacked_x = [stacked_x; x_and_u];
-        stacked_y = [stacked_y; data(n).y(2:end,:)];
-    end
-
-    train_test_cutoff = ceil(size(stacked_x,1)/2);
+    x_additive_noise = bsxfun(@times, randn(size(stacked_y)), noise_std_multiplier*obs_noise_stds);
+    y_additive_noise = bsxfun(@times, randn(size(stacked_y)), noise_std_multiplier*obs_noise_stds);
+    stacked_x_noisy(:,1:end-2) = stacked_x_noisy(:,1:end-2) + x_additive_noise;
+    stacked_y_noisy = stacked_y_noisy + y_additive_noise;
     
-    train_x = stacked_x(1:train_test_cutoff,:);
-    train_y = stacked_y(1:train_test_cutoff,:);
-    test_x = stacked_x(train_test_cutoff+1:end,:);
-    test_y = stacked_y(train_test_cutoff+1:end,:);
+    train_x = stacked_x_noisy(1:train_test_cutoff,:);
+    train_y = stacked_y_noisy(1:train_test_cutoff,:);
+    test_x = stacked_x_noisy(train_test_cutoff+1:end,:);
+    test_y = stacked_y_noisy(train_test_cutoff+1:end,:);
     
     
     % GP fitting:
+    
+    meanfunc = {@meanSum, {@meanLinear, @meanConst}};          
+    covfunc = @covSEard;             
+    likfunc = @likGauss;  
     
     out_dims = size(test_y,2);
     
@@ -72,6 +90,12 @@ for noise_std_multiplier = noise_std_multipliers
         [preds(:,out_dim), pred_stds(:,out_dim)] = gp(hyp_fit{out_dim}, @infGaussLik, meanfunc, covfunc, likfunc, ...
             train_x, train_y(:,out_dim), test_x);
     end
+    
+    fit_noise_levels = [hyp_fit{:}];
+    fit_noise_levels = exp([fit_noise_levels(:).lik])
+    true_noise_levels = noise_std_multiplier*obs_noise_stds
+    sqrt2_inflated_true_noise_levels = true_noise_levels*sqrt(2)
+    sqrt3_inflated_true_noise_levels = true_noise_levels*sqrt(3)
 
     save(strrep(sprintf('results/noise_level_%g', noise_std_multiplier),'.','_'))
 end
--- plotter.m
+++ plotter.m
@@ -6,5 +6,11 @@ for out_dim = 1:5
     figure;
     f = [preds(:,out_dim)+2*sqrt(pred_stds(:,out_dim)); flipdim(preds(:,out_dim)-2*sqrt(pred_stds(:,out_dim)),1)];
     fill([[1:n_test_points]'; flipdim([1:n_test_points]',1)], f, [7 7 7]/8)
-    hold on; plot(1:n_test_points, preds(:,out_dim)); plot(1:n_test_points, test_y(:,out_dim))
+    hold on; plot(1:n_test_points, preds(:,out_dim),'r-o'); 
+    plot(1:n_test_points, test_x(:,out_dim),'b-o')
+    plot(1:n_test_points, test_y(:,out_dim),'y-o')
+    try
+        plot(1:n_test_points, test_y_noiseless(:,out_dim),'g-o')
+    catch
+    end
 end
