9b460df2e4f0087d1cb8dba4472c970cfe16e309
Carl Edward Rasmussen <cer54@cam.ac.uk>
2017-05-15 11:27:23 +0100

    added test partition

--- generate_training_data.m
+++ generate_training_data.m
@@ -11,13 +11,13 @@ plant.dt = 0.075;
 plant.ctrltype = @(t,f,f0)zoh(t,f,f0);               
 plant.ode = @dynamics;
 
-% Noise s.d.'s: 1cm, 1 degree
+% Noise s.d.'s: 4cm, 4 degree
 % Dimensions: x position, sin(\theta), cos(\theta), cart velocity, angular velocity
 obs_noise_std = 4*[0.01 pi/180 pi/180 0.01/plant.dt pi/180/plant.dt]; % add noise to sin & cos independently
 add_noise = true;
 
 N = 10;
-discard_first_n_steps = 0;
+discard_first_n_steps = 1;
 T = 100 + discard_first_n_steps;
 max_force = 5;
 x0 = [0,0,0,0];
@@ -26,7 +26,7 @@ u = cell(1,N);
 y_noiseless = cell(1,N);
 y = cell(1,N);
 
-for n=1:N
+for n=1:2*N
     n
     u{n} = 2*max_force*(rand(T-1,1)-0.5);
     
@@ -46,8 +46,10 @@ for n=1:N
     y_n(:,5) = cos(y_n(:,2));
     y_n(:,2) = sin(y_n(:,2));
     y_n = y_n(:,[1,2,5,3,4]); 
-    
-    y_n = y_n(discard_first_n_steps+1:end,:);
+
+    if any(abs(y_n(:,1))>1), t = min(find(abs(y_n(:,1))>1))-1; else t = T; end
+    y_n = y_n(discard_first_n_steps+1:t,:);
+    u{n} = u{n}(discard_first_n_steps+1:t-1,:);
     
     y_noiseless{n} = y_n;
     if add_noise
@@ -59,8 +61,10 @@ end
 
 y_position_angle = cellfun(@(x)({x(:,1:3)}),y);
 
-data = struct('y',y,'u',u,'latent',y_noiseless);
-data_position_angle = struct('y',y_position_angle,'u',u,'latent',y_noiseless);
+train = struct('y', y(1:N),     'u', u(1:N),     'latent', y_noiseless(1:N));
+test  = struct('y', y(N+1:2*N), 'u', u(N+1:2*N), 'latent', y_noiseless(1+N:2*N));
+train_position_angle = struct('y', y_position_angle(1:N),     'u', u(1:N),     'latent', y_noiseless(1:N));
+test_position_angle  = struct('y', y_position_angle(N+1:2*N), 'u', u(N+1:2*N), 'latent', y_noiseless(N+1:2*N));
 
-save('data_all', 'data')
-save('data_position_angle', 'data_position_angle')
+save('data_all', 'train', 'test')
+save('data_position_angle', 'train_position_angle', 'test_position_angle')
